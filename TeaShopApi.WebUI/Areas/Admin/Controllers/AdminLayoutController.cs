33f9d8a273b957ab6ae0842172e5242ff161a09a
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-11-28 22:17:27 +0300

    Add Admin Panel

---
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/AdminLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TeaShopApi.WebUI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class AdminLayoutController : Controller
+    {       
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        }
+
+		public PartialViewResult PartialSidebar()
+		{
+			return PartialView();
+		}
+	}
+}
